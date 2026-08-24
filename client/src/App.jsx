e7363a14c13a26bab7b2cc1609bafddcb05408c1
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-08-24 17:32:09 +0200

    Added preset bg and txt colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-white transition-colors">
-      <header className="p-4 flex justify-between items-center max-w-4xl mx-auto">
+    <div className="min-h-dvh transition-colors">
+      <header className="p-4 flex justify-between items-center mx-auto">
         <h1 className="text-xl font-bold text-emerald-600 dark:text-emerald-400">
           InfoFynd
         </h1>
@@ -23,10 +23,8 @@ export default function App() {
       <main className="max-w-4xl mx-auto p-4">
         <h1 className="text-3xl font-bold text-emerald-600 dark:text-emerald-400">
           InfoFynd
-        </h1>{" "}
-        <p className="dark:text-gray-300 text-black">
-          Svar från servern: {message || "Laddar..."}
-        </p>
+        </h1>
+        <p>Svar från servern: {message || "Laddar..."}</p>
       </main>
     </div>
   );
