d785676cb72186cc11d109fe6eee1a9b30b58c3e
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-09-14 12:31:13 +0200

    server.js updated for publication

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import db from "./data/db.js";
 import postRoutes from "./routes/postRoutes.js";
 import memberRoutes from "./routes/memberRoutes.js";
-import db from "./data/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -14,11 +19,17 @@ app.use("/api/members", memberRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admins", adminRoutes);
 
-const port = process.env.PORT || 8000;
-
 app.get("/api", (req, res) => {
   res.send("Hello World! how you doing?");
 });
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
